ab7e80396f5ac1391ef7e017dfd28dc1d8f16de9
Myron <mossjmyron@gmail.com>
2022-10-24 03:34:12 -0400

    Added search functionality

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -1,34 +1,73 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { data } from "../test/TestData";
 import AutomationCard from "../components/AutomationCard";
-import { Grid, Stack, Chip, Paper } from "@mui/material";
+import {
+  Grid,
+  Stack,
+  Chip,
+  TextField,
+  InputAdornment,
+  Box,
+} from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 const Explore = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <Fragment>
-      {data.map((item) => (
-        <Grid container padding={1}>
-          <AutomationCard
-            key={item.index}
-            image={item.image}
-            title={item.title}
-            description={item.description}
-            labels={item.labels.map((item) => (
-              <Stack key={item.key} direction="row" spacing={2}>
-                <Chip
-                  label={item.label}
-                  style={{ backgroundColor: item.color }}
-                />
-              </Stack>
-            ))}
-            score={item.score}
-            name={item.name}
-            role={item.role}
-            occupation={item.occupation}
-            profile={item.profile}
-          />
-        </Grid>
-      ))}
+      <Box component="form">
+        <TextField
+          onChange={(e) => setSearch(e.target.value)}
+          variant="filled"
+          size="small"
+          margin="normal"
+          label="Search"
+          type="search"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      <Grid container padding={1} spacing={1.5}>
+        {data
+          .filter((data) => {
+            if (data === "") return false;
+            if (
+              data.description
+                .toLowerCase()
+                .includes(search.toLocaleLowerCase())
+            )
+              return true;
+          })
+          .map((data) => (
+            <Grid item key={data.index}>
+              <AutomationCard
+                image={data.image}
+                title={data.title}
+                description={data.description}
+                labels={data.labels.map((data) => (
+                  <Stack key={data.key} direction="row" spacing={2}>
+                    <Chip
+                      label={data.label}
+                      style={{ backgroundColor: data.color }}
+                    />
+                  </Stack>
+                ))}
+                score={data.score}
+                name={data.name}
+                role={data.role}
+                occupation={data.occupation}
+                profile={data.profile}
+              />
+            </Grid>
+          ))}
+      </Grid>
     </Fragment>
   );
 };
